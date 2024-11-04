d66dcc5b1c151674cb8540de4e2e0f6b63932b05
WittyCrab <thomas.jpr.vigier@gmail.com>
2024-11-04 15:26:30 +0800

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,17 +1,16 @@
 import requests
+import json 
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyse } }
-
-    returned_value = requests.post(url,headers=header, json=input_json)
-
-      formatted_response = json.loads(response.text)
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
 
     if response.status_code == 200:
         emotion = formatted_response['emotionPredictions'][0]['emotion']
         max_emotion = max(emotion.items(), key=lambda x: x[1])
-        return max_emotion[0]
+        print max_emotion[0]
     else:
         return None
